54dc94e6c9755699109d8dfed102d55337831267
Alok3333 <alokprajapati697@gmail.com>
2025-01-01 15:28:56 +0530

    created modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import Modal from "./components/Modal";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  // Handle for open form modal
+  const handleOpen = () => {
+    setOpen(true);
+  };
   return (
     <div className="App">
-     super javascript
+      <center>
+        <h1>User Details Modal</h1>
+        <button onClick={handleOpen} className="openBtn">Open Form</button>
+        {open && <Modal setIsOpen={setOpen} />}
+      </center>
     </div>
   );
 }
